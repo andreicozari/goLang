da49f2f1e2fb4979ad0da33f6b5e3ad3533206e6
Andrei Cozari <extern02@mik-it.de>
2020-04-01 17:31:01 +0300

    export GO_SERVICE_PORT=9090

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
 func main() {
@@ -10,10 +11,19 @@ func main() {
 
 	http.HandleFunc("/", index)
 
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(port(), nil)
 }
 
 func index(writer http.ResponseWriter, request *http.Request) {
 	writer.WriteHeader(http.StatusOK)
 	fmt.Fprint(writer, "Hello from a microservice written in GO lang")
+}
+
+func port() string {
+	port := os.Getenv("GO_SERVICE_PORT")
+
+	if len(port) == 0 {
+		port = ":8080"
+	}
+	return port
 }
