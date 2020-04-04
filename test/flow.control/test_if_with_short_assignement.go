6fd7cd4c0a93c40591ebb6e1d25ec35791340ec1
Andrei Cozari <extern02@mik-it.de>
2020-04-04 15:59:40 +0300

    Test pointers. Move flow control examples in separate package.

--- test/flow.control/test_if_with_short_assignement.go
+++ test/flow.control/test_if_with_short_assignement.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 
-	if val := rand.Intn(30); val < 100 {
+	if val := rand.Intn(122); val < 100 {
 		fmt.Printf("%v Is less than 100", val)
 	} else {
 		fmt.Printf("%v Is greater than 100", val)
