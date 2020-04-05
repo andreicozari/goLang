c5913161a7a8ca6731db03e3e211275643034234
Andrei Cozari <extern02@mik-it.de>
2020-04-06 00:41:54 +0300

    Random seeded by time to get unpredictable results.

--- test/flow.control/test_if_with_short_assignement.go
+++ test/flow.control/test_if_with_short_assignement.go
@@ -11,7 +11,6 @@ func main() {
 	// Seed the random number generator using the current time (nanoseconds since epoch):
 	rand.Seed(time.Now().UnixNano())
 
-	//if val := rand.Intn(122); val < 100 {
 	if val := rand.Intn(200); val < 100 {
 		fmt.Printf("%v Is less than 100", val)
 	} else {
