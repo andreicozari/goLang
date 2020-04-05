ea02e88588e29d85c7b48483331933d40365e4a0
Andrei Cozari <extern02@mik-it.de>
2020-04-06 00:40:55 +0300

    Test random seeded by time to get unpredictable results. Can be guessed depending by knowing when it was run/ executed.

--- test/flow.control/test_if_with_short_assignement.go
+++ test/flow.control/test_if_with_short_assignement.go
@@ -3,11 +3,16 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"time"
 )
 
 func main() {
 
-	if val := rand.Intn(122); val < 100 {
+	// Seed the random number generator using the current time (nanoseconds since epoch):
+	rand.Seed(time.Now().UnixNano())
+
+	//if val := rand.Intn(122); val < 100 {
+	if val := rand.Intn(200); val < 100 {
 		fmt.Printf("%v Is less than 100", val)
 	} else {
 		fmt.Printf("%v Is greater than 100", val)
