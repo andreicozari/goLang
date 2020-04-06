11472d2b0c3e059b2025bccad9878449565587fb
Andrei Cozari <extern02@mik-it.de>
2020-04-06 18:37:13 +0300

    Test the empty interface that can hold any type of value.

--- test/interfaces/empty/test-empty-interface.go
+++ test/interfaces/empty/test-empty-interface.go
@@ -24,4 +24,6 @@ func main() {
 	i = "stringValue"
 	fmt.Println(i)
 
+	fmt.Print()
+
 }
--- test/interfaces/nil/test_nil_interface.go
+++ test/interfaces/nil/test_nil_interface.go
@@ -8,13 +8,13 @@ A nil interface value holds neither value nor concrete type.
 Calling a method on a nil interface is a run-time error because there is no type inside the interface tuple to indicate which concrete method to call.
 */
 
-type MyIterface interface {
+type MyInterface interface {
 	method()
 }
 
 func main() {
 
-	var myInterface MyIterface
+	var myInterface MyInterface
 
 	fmt.Printf("Show the (value; type) of nil interface: (%v , %T) \n",
 		myInterface, myInterface)
