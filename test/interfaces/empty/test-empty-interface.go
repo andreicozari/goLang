966c6a2a0c8a38a7e6fa4da34a3892d0dad2a361
Andrei Cozari <extern02@mik-it.de>
2020-04-06 18:32:57 +0300

    Test the empty interface that can hold any type of value.

---
+++ test/interfaces/empty/test-empty-interface.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+/*
+
+The empty interface
+The interface type that specifies zero methods is known as the empty interface:
+interface{}
+An empty interface may hold values of any type. (Every type implements at least zero methods.)
+Empty interfaces are used by code that handles values of unknown type. For example, fmt.Print takes any number of arguments of type interface{}.
+
+*/
+
+func main() {
+	var i interface{}
+
+	// print the nil interface:
+	fmt.Println(i)
+
+	i = 1
+	fmt.Println(i)
+
+	i = "stringValue"
+	fmt.Println(i)
+
+}
