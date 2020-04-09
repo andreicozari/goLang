a769acb5d7f87660d1d66ad2db1f5c587928bd43
Andrei Cozari <extern02@mik-it.de>
2020-04-10 00:42:49 +0300

    Play with buffered vs unbuffered channels to see the order of the sending and receiving call order.
    Without channels, Go can't guaranty the order of calls for goR.
    And we can't use func return values in go statement.

---
+++ test/goroutine/test-goroutine-with-channel.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	strChan := make(chan string)
+
+	// try with buffered chan:
+	//strChan := make(chan string, 3)
+
+	go abc(strChan)
+
+	fmt.Println("\n Received ", <-strChan)
+	fmt.Println("\n Received ", <-strChan)
+	fmt.Println("\n Received ", <-strChan)
+
+	/*for ; ; {
+		fmt.Println("\n Received ",  <- strChan)
+
+		for _, r := range `12345` {
+			fmt.Printf("\r%c", r)
+			time.Sleep(1000 * time.Millisecond)
+		}
+	}*/
+}
+
+func abc(strChan chan string) {
+
+	fmt.Println("\n sending a")
+	strChan <- "a"
+	fmt.Println("\n sent a")
+	//time.Sleep(500 * time.Millisecond)
+
+	fmt.Println("\n sending b")
+	strChan <- "b"
+	fmt.Println("\n send b")
+	//time.Sleep(500 * time.Millisecond)
+
+	fmt.Println("\n sending c")
+	strChan <- "c"
+	fmt.Println("\n sent c")
+	//time.Sleep(500 * time.Millisecond)
+}
