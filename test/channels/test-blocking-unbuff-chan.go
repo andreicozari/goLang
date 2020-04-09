1a7caf935df937c45de1260a3347cfc8991fda41
Andrei Cozari <extern02@mik-it.de>
2020-04-09 19:51:27 +0300

    The go func --> lunches a new anonymous go routine / a closure as anonymous function, 	This goR is pushing a int value of 1 into the unbuffered channel.
    	The main goR then read from the channel and prints the value.

--- test/channels/test-blocking-unbuff-chan.go
+++ test/channels/test-blocking-unbuff-chan.go
@@ -1,11 +1,22 @@
 package main
 
+import "fmt"
+
+/*
+	The go func --> lunches a new anonymous go routine / a closure as anonymous function,
+	This goR is pushing a int value of 1 into the unbuffered channel.
+
+	The main goR then read from the channel and prints the value.
+*/
 func main() {
 	unBuffChan := make(chan int)
 
-	// blocked operation / can't read any value because the channel has no values
-	// You can't read from an unbuffered channel if until now nobody have pushed any value
-	// A deadlock here:
-	<-unBuffChan
-	//fmt.Println("After trying to get a value from an unbuffered channel without being pushed any value in that channel!")
+	go func() {
+		// push a int value to the unbuffered channel:
+		unBuffChan <- 1
+	}()
+
+	// receive / get / read a int value from the unbuffered channel:
+	intVal := <-unBuffChan
+	fmt.Println(intVal)
 }
