dc29e6dc28a52012518e106db9d04e5c856c9bef
Andrei Cozari <extern02@mik-it.de>
2020-04-10 02:28:43 +0300

    Test goroutine communication with channels. If we don't know the number of data in a channel, we use select statement: select can listen for data incoming in channel.

---
+++ test/channels/select/test-select.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type Word struct {
+	val    string
+	length int
+}
+
+func NewWord(val string) Word {
+	return Word{val: val}
+}
+
+func (word Word) String() string {
+	return fmt.Sprintf("{%v, %v}", word.val, word.length)
+}
+
+func main() {
+	//strChan := make(chan string)
+
+	// try with buffered chan:
+	wordsChan := make(chan Word, 3)
+
+	valuesToSend := []Word{NewWord("One"), NewWord("Three"), NewWord("Four")}
+
+	// send concurrently:
+	for _, val := range valuesToSend {
+		go sendValToChan(val, wordsChan)
+	}
+
+	// what if we don't know the number of data in a channel ?
+	// Let's say: we don't know how many goroutines was started
+	/*for i := 0; i < len(valuesToSend); i++ {
+		receive(wordsChan)
+	}*/
+
+	// we use select statement:
+	// select can listen for data incoming in channel:
+	select {}
+}
+
+func sendValToChan(word Word, wordsChan chan Word) {
+	word.length = len(word.val)
+	fmt.Println("\n sending ", word)
+	time.Sleep(time.Second)
+	wordsChan <- word
+
+	// the next line will be executed concurrently with the receiver
+	fmt.Println("\n sent ", word)
+}
+
+func receive(wordsChan chan Word) {
+	fmt.Println("Received ", <-wordsChan)
+}
