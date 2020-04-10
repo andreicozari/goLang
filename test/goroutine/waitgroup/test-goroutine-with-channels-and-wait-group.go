d359a61ff258b599bdfcb7e23206cb0484e9896f
Andrei Cozari <extern02@mik-it.de>
2020-04-10 03:25:37 +0300

    Test wait group --> avoid sleep time

---
+++ test/goroutine/waitgroup/test-goroutine-with-channels-and-wait-group.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var wg sync.WaitGroup
+
+func main() {
+	//strChan := make(chan string)
+
+	// try with buffered chan:
+	strChan := make(chan string, 3)
+
+	go sendABC(strChan)
+
+	for i := 0; i < 3; i++ {
+		receive(strChan)
+	}
+
+}
+
+func sendABC(strChan chan string) {
+
+	fmt.Println("\n sending a")
+	strChan <- "a"
+	fmt.Println("\n sent a")
+	//time.Sleep(500 * time.Millisecond)
+
+	fmt.Println("\n sending b")
+	strChan <- "b"
+	fmt.Println("\n send b")
+	//time.Sleep(500 * time.Millisecond)
+
+	fmt.Println("\n sending c")
+	strChan <- "c"
+	fmt.Println("\n sent c")
+	//time.Sleep(500 * time.Millisecond)
+}
+
+func receive(strChan chan string) {
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		fmt.Println("\n Received ", <-strChan)
+	}()
+	wg.Wait()
+}
