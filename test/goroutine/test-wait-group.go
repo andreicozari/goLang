0296d63a2ded9237fd36c7cf6dd6fa0a9a50e461
Andrei Cozari <extern02@mik-it.de>
2020-04-10 03:09:52 +0300

    Test wait group --> blocks the main GoR until the second GoR is completed without blocking. Is good fit to avoid sleep in duration of time.

---
+++ test/goroutine/test-wait-group.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	var wg sync.WaitGroup
+	salutation := "hello"
+	wg.Add(1)
+
+	fmt.Println(salutation)
+
+	go func() {
+		defer wg.Done()
+		salutation = "welcome"
+	}()
+
+	wg.Wait()
+	fmt.Println(salutation)
+
+}
