5b962a6f2f710ac67e4d746ad4e81e7522019c9d
Andrei Cozari <extern02@mik-it.de>
2020-04-10 11:30:36 +0300

    Test - generate sequence with select statement

---
+++ test/channels/select/test-generate-sequence-with-select.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	sequenceChannel := make(chan interface{}, 24)
+
+	go push(sequenceChannel)
+
+	for char := range sequenceChannel {
+		fmt.Print(char)
+	}
+}
+
+func push(ch chan interface{}) {
+	for i := 0; i < cap(ch); i++ {
+		// the select is used the operation over the channels:
+		// only one case will be chosen at a time/ like switch operation:
+		select {
+		case ch <- "a":
+		case ch <- "b":
+		case ch <- "c":
+		case ch <- "d":
+		case ch <- "e":
+		case ch <- "f":
+		case ch <- "1":
+		case ch <- "2":
+		case ch <- "3":
+		case ch <- "4":
+		case ch <- "5":
+		case ch <- "6":
+		}
+	}
+
+	// Notify the receiver that there are no more values in the channel:
+	close(ch)
+}
