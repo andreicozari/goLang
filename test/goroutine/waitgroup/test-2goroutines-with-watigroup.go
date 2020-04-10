d202bc722297c569c5392036cecc6a67f0a29f51
Andrei Cozari <extern02@mik-it.de>
2020-04-10 03:46:58 +0300

    Test 2 goroutines with wait group

---
+++ test/goroutine/waitgroup/test-2goroutines-with-watigroup.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func main() {
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		fmt.Println("1st goroutine sleeping...")
+		time.Sleep(1)
+		fmt.Println("1st goroutine woke Up")
+	}()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		fmt.Println("2nd goroutine sleeping...")
+		time.Sleep(2)
+		fmt.Println("2nd goroutine woke Up")
+	}()
+	wg.Wait()
+
+	fmt.Println("All goroutines complete.")
+
+}
