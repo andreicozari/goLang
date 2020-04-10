c544b69440f7ae7c7d5aa552a3eb95ccbea28d62
Andrei Cozari <extern02@mik-it.de>
2020-04-10 11:58:09 +0300

    Test --> tell the main goR to wait by using waitGroup.wait() instead of a delay with time sleep. Next refactor --> use again two secondary goRs concurrently with waitGroup mechanism

--- test/channels/waitgroup/test-wait-group-for-2-goroutines.go
+++ test/channels/waitgroup/test-wait-group-for-2-goroutines.go
@@ -2,14 +2,36 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
 func main() {
-	go showNTimesAWord(2, "Apple")
-	go showNTimesAWord(3, "Banana")
+	// Define the wait group counter
+	var waitGroupCounter sync.WaitGroup
 
-	time.Sleep(time.Second)
+	// Add one goroutine to the  WaitGroup counter:
+	// I have one goR to wait for:
+	waitGroupCounter.Add(1)
+
+	// Then i need to decrement the counter --> to tell when no need to wait more for the goR:
+	// Instead of calling directly the go showNTimesAWord , we will use a go call to a closure
+	//  for handling the WaitGroup counter decrement
+
+	go func() {
+		showNTimesAWord(2, "Apple")
+
+		// decrement wg counter:
+		waitGroupCounter.Done()
+	}()
+
+	//go showNTimesAWord(3, "Banana")
+
+	// get rid of sleep time
+	// we don't need any more, a delay here, to wait for the others goRs to execute / to finish
+	//time.Sleep(time.Second)
+	// And use here a wg.wait() instead a delay with sleep time:
+	waitGroupCounter.Wait()
 }
 
 func showNTimesAWord(n int, str string) {
