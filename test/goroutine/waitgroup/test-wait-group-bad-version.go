9ab8ed50b377c32a12a024f31ba51ba9b549b1bb
Andrei Cozari <extern02@mik-it.de>
2020-04-12 16:59:14 +0300

    Not synchronized mainGoR with it's subRoutines / goRoutines. The loop index value is lost when sending from mainGoR to sub goRs. 1 main stack + 9 subGoRs stack = 10 stacks. Depending on timing --> only few values have chances to be synchronize between stacks.

--- test/goroutine/waitgroup/test-wait-group-bad-version.go
+++ test/goroutine/waitgroup/test-wait-group-bad-version.go
@@ -15,11 +15,10 @@ func main() {
 		fmt.Println(val)
 	}*/
 
-	// the for loop is in the the mainGoR and it's i index is not synchronized inside
-	// and the goR is has another / it's own stack, so --> we don't know at each for loop iteration
+	// the for loop is in the the mainGoR and it's i index is not synchronized next inside goR
+	// and the goR has another / it's own stack, so --> we don't know at each for loop iteration
 	// which value will have var i, and even the for loop can finish sooner that all 9 loop iterations:
 	for i := 1; i <= 9; i++ {
-		wg.Add(1)
 		go func() {
 			wg.Add(1)
 			defer wg.Done()
