cb085429de3ae9b2379cb23f72a60b8043f3de3c
Andrei Cozari <extern02@mik-it.de>
2020-04-12 17:20:34 +0300

    Without passing the i var to goRoutines --> there is a high probability the loop will exit	before the goroutines are begun

--- test/goroutine/waitgroup/test-wait-group-bad-version.go
+++ test/goroutine/waitgroup/test-wait-group-bad-version.go
@@ -18,6 +18,8 @@ func main() {
 	// the for loop is in the the mainGoR and it's i index is not synchronized next inside goR
 	// and the goR has another / it's own stack, so --> we don't know at each for loop iteration
 	// which value will have var i, and even the for loop can finish sooner that all 9 loop iterations:
+	/* without passing the i var to goRoutines --> there is a high probability the loop will exit
+	before the goroutines are begun*/
 	for i := 1; i <= 9; i++ {
 		go func(i int) {
 			wg.Add(1)
